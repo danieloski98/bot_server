16e12dfe0241b1f6b2fba427a9843fa551f474eb
NEON <danielemmanuel257@gmail.com>
2020-12-20 10:53:03 +0100

    removed warning from accesspage

--- src/Components/Dashboard/Components/AcessSettings.tsx
+++ src/Components/Dashboard/Components/AcessSettings.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Table, Thead, Tbody, Tr, Th, Td, TableCaption } from "@chakra-ui/react"
+import { Table, Thead, Tbody, Tr, Th, Td, } from "@chakra-ui/react"
 
 export default function AcessSettings() {
     return (
